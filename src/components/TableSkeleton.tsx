474916cd32803b37533646b4cdf2834068ec8fc0
Gourav soni <gouravsoni8476@gmail.com>
2024-10-07 13:56:46 +0530

    fixed home page skeleton

--- src/components/TableSkeleton.tsx
+++ src/components/TableSkeleton.tsx
@@ -2,16 +2,16 @@ import type React from "react";
 
 const TableSkeleton: React.FC = () => {
 	return (
-		<div className="w-full px-8 bg-slate-700 h-screen text-white items-center rounded z-20">
-			<div className="animate-pulse py-44">
-				<div className="bg-gray-300 h-10 w-full mb-4 rounded" />
+		<div className="w-full px-8 bg-slate-800 h-screen text-white items-center rounded z-20">
+			<div className="animate-pulse md:w-1/2 md:mx-auto py-44">
+				<div className="bg-gray-400 h-10 w-full mb-4 rounded" />
 				{[...Array(5)].map(() => (
 					<div
 						key={`row-${crypto.randomUUID()}`}
 						className="flex justify-center space-x-4 mb-4"
 					>
-						<div className="bg-gray-300 h-8 w-1/2 rounded" />
-						<div className="bg-gray-300 h-8 w-1/2 rounded" />
+						<div className="bg-gray-400 h-8 w-1/2 rounded" />
+						<div className="bg-gray-400 h-8 w-1/2 rounded" />
 					</div>
 				))}
 			</div>
